7c11af8dea06e110a7a0fb77ae8bf3db039e223d
David Samson <david.andrew.engineer@gmail.com>
2022-12-05 00:14:53 -0500

    added month names to example

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,51 @@ use gloo_file::{Blob, callbacks::FileReader};
 use web_sys::{Event, HtmlInputElement, DragEvent};
 use log::info;
 
+// [Tasks]
+// 1. draggable cards
 
-// 1. convert readers list to a set and remove reader when done
-// 2. use better input as input instead of button
+// > 1...
+// move onchange/ondrop/ondragover into the Input component
+// replace list of files with a list of structs representing each card
+//  - picture data string
+//  - cropping bounds/other transforms
+// separate list for storing any text alterations per each month
+//  - holiday names
+//  - include lunar phases
+//  - poem (TODO: needs to be clear that this stays with the month when you drag the picture... perhaps a second draggable list that matches the pictures?)
+// draggable/orderable cards in the list
+// ability to type in the text for each card
+// render to PDF process -> opens in a new window!
+//   possibly save editor state to local storage?
+
+// convert file loading example to functional style, and probably package as a hook
+// card component to hold picture for each month + optional spot for text
+// maybe find some sort of image editor type thing? mainly for cropping images
+
+
+//[Design Notes]
+// picking which holidays are included and their names should be handled in a modal
+// ---> basically all global settings will be from a settings modal
+
 
 
 #[function_component(App)]
 fn app() -> Html {
+
+    let months: Vec<String> = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ].iter().map(|s| s.to_string()).collect();
 
     //TODO: how to have readers be dropped when they finish adding the image to the list?
     //perhaps have a set of readers, and then remove from the set?
@@ -105,10 +143,10 @@ fn app() -> Html {
             <div class="bg-blue-500 h-20 flex items-center justify-center text-5xl text-white">{"Holocene Calendar Maker"}</div>
             // <input type="file" multiple=true onchange={onchange} accept="image/*" />
             <FileInput onchange={onchange} ondragover={ondragover} ondrop={ondrop} />
-            <div class="flex">
-                { for images.current().iter().map(|data| 
+            <div class="flex flex-wrap">
+                { for images.current().iter().zip(months).map(|(data, month)| 
                     html! { 
-                        <Card src={data.clone()} />
+                        <Card src={data.clone()} title={month} />
                     }
                 ) }
             </div>
@@ -124,19 +162,24 @@ fn main() {
 #[derive(Properties, PartialEq)]
 struct CardProps {
     src: String,
+    title: String,
+    poem: Option<String>,
 }
 
 #[function_component(Card)]
 fn card(props: &CardProps) -> Html {
+
+    let CardProps {src, title, poem} = props;
+
     //simple card component with a picture and a body for text
     html! {
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
-                <img class="rounded-t-lg" src={props.src.clone()} alt="" />
+                <img class="rounded-t-lg" src={src.clone()} alt="" />
             </a>
             <div class="p-5">
                 <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{"Noteworthy technology acquisitions 2021"}</h5>
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title.clone()}</h5>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     {"Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order."}
@@ -155,11 +198,9 @@ fn card(props: &CardProps) -> Html {
 
 #[derive(Properties, PartialEq)]
 struct InputProps {
-    // multiple: Option<bool>,
     onchange: Callback<Event>,
     ondrop: Callback<DragEvent>,
     ondragover: Callback<DragEvent>,
-    // accept: String //="image/*" 
 }
 #[function_component(FileInput)]
 fn file_input(props: &InputProps) -> Html {
@@ -180,150 +221,3 @@ fn file_input(props: &InputProps) -> Html {
     } 
 }
 
-
-//hooks to grab
-//use_drop -> for sorting the pictures by month
-//
-
-//[Tasks]
-// display list of pictures loaded in from browse file
-// browse file button should have no state (i.e. not display file name/number selected).
-//    also the button should say something like "add pictures"
-// replace list of files with a list of structs representing each card
-// draggable/orderable cards in the list
-// ability to type in the text for each card
-// render to PDF process -> opens in a new window!
-//   possibly save editor state to local storage?
-
-// convert file loading example to functional style, and probably package as a hook
-// card component to hold picture for each month + optional spot for text
-// maybe find some sort of image editor type thing? mainly for cropping images
-
-
-
-
-
-// use std::collections::HashMap;
-// use yew::{html, html::TargetCast, Component, Context, Html};
-// use gloo_file::callbacks::FileReader;
-
-// type Chunks = bool;
-
-// pub enum Msg {
-//     Loaded(String, String),
-//     LoadedBytes(String, Vec<u8>),
-//     Files(Vec<File>, Chunks),
-//     ToggleReadBytes,
-// }
-
-// pub struct Model {
-//     readers: HashMap<String, FileReader>,
-//     files: Vec<String>,
-//     read_bytes: bool,
-// }
-
-// impl Component for Model {
-//     type Message = Msg;
-//     type Properties = ();
-
-//     fn create(_ctx: &Context<Self>) -> Self {
-//         Self {
-//             readers: HashMap::default(),
-//             files: vec![],
-//             read_bytes: false,
-//         }
-//     }
-
-//     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
-//         match msg {
-//             Msg::Loaded(file_name, data) => {
-//                 let info = format!("file_name: {}, data: {:?}", file_name, data);
-//                 self.files.push(info);
-//                 self.readers.remove(&file_name);
-//                 true
-//             }
-//             Msg::LoadedBytes(file_name, data) => {
-//                 let info = format!("file_name: {}, data: {:?}", file_name, data);
-//                 self.files.push(info);
-//                 self.readers.remove(&file_name);
-//                 true
-//             }
-//             Msg::Files(files, bytes) => {
-//                 for file in files.into_iter() {
-//                     let file_name = file.name();
-//                     let task = {
-//                         let file_name = file_name.clone();
-//                         let link = ctx.link().clone();
-
-//                         if bytes {
-//                             gloo_file::callbacks::read_as_bytes(&file, move |res| {
-//                                 link.send_message(Msg::LoadedBytes(
-//                                     file_name,
-//                                     res.expect("failed to read file"),
-//                                 ))
-//                             })
-//                         } else {
-//                             gloo_file::callbacks::read_as_text(&file, move |res| {
-//                                 link.send_message(Msg::Loaded(
-//                                     file_name,
-//                                     res.unwrap_or_else(|e| e.to_string()),
-//                                 ))
-//                             })
-//                         }
-//                     };
-//                     self.readers.insert(file_name, task);
-//                 }
-//                 true
-//             }
-//             Msg::ToggleReadBytes => {
-//                 self.read_bytes = !self.read_bytes;
-//                 true
-//             }
-//         }
-//     }
-
-//     fn view(&self, ctx: &Context<Self>) -> Html {
-//         let flag = self.read_bytes;
-//         html! {
-//             <div>
-//                 <div>
-//                     <p>{ "Choose a file to upload to see the uploaded bytes" }</p>
-//                     <input type="file" multiple=true onchange={ctx.link().callback(move |e: Event| {
-//                             let mut result = Vec::new();
-//                             let input: HtmlInputElement = e.target_unchecked_into();
-
-//                             if let Some(files) = input.files() {
-//                                 let files = js_sys::try_iter(&files)
-//                                     .unwrap()
-//                                     .unwrap()
-//                                     .map(|v| web_sys::File::from(v.unwrap()))
-//                                     .map(File::from);
-//                                 result.extend(files);
-//                             }
-//                             Msg::Files(result, flag)
-//                         })}
-//                     />
-//                 </div>
-//                 <div>
-//                     <label>{ "Read bytes" }</label>
-//                     <input type="checkbox" checked={flag} onclick={ctx.link().callback(|_| Msg::ToggleReadBytes)} />
-//                 </div>
-//                 <ul>
-//                     { for self.files.iter().map(|f| Self::view_file(f)) }
-//                 </ul>
-//             </div>
-//         }
-//     }
-// }
-
-// impl Model {
-//     fn view_file(data: &str) -> Html {
-//         html! {
-//             <li>{ data }</li>
-//         }
-//     }
-// }
-
-// // fn main() {
-// //     yew::start_app::<Model>();
-// // }
